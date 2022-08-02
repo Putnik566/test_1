c584fd57b7e348da4de69d7c360c5b39b24dd4bc
Eg012 <93487296+Eg012@users.noreply.github.com>
2022-08-03 00:31:27 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,19 @@
+print("Hi i from Russian  !!!")
+print("Russia is >>>> GOOD Country")
+
+#yggyghghghhhhhh
+import random
+
+def Run():
+    resulf = f'Name: {}, Age: {age_user_win}'
+    user_win_list = ["Egor"]
+
+def RunGreating():
+    pass
+
+def Run_film():
+    pass
+#yggyghghghhhhh
+# hi i am Egor Savinov !!!
+#|||||||||||||||||||||||||||
+
